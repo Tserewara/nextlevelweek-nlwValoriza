d8f104a5288d77ee2c0fd5c49ef0b80c65688cec
Tserewara <alvaro.alf2@gmail.com>
2021-06-26 09:26:00 -0300

    Add env var to select between development and production in ormconfig.js

--- ormconfig.js
+++ ormconfig.js
@@ -1,14 +1,33 @@
-module.exports = {
+DEVELOPMENT = {
+  "type": "postgres",
+  "host": "localhost",
+  "username": "postgres",
+  "password": "xavante",
+  "port": 5432,
+  "database": "nlw-valoriza-db",
+  "migrations": ["src/database/migrations/*.ts"],
+  "entities": ["src/entities/*.ts"],
+  "cli": {
+    "migrationsDir": "src/database/migrations",
+    "entitiesDir": "src/entities"
+  }
+}
+
+PRODUCTION = {
   "type": "postgres",
   "host": "nlw-valoriza-db",
   "username": "postgres",
   "password": process.env.POSTGRES_PASSWORD,
   "port": 5432,
   "database": "nlw-valoriza-db",
-  "migrations": ["dist/database/migrations/*.js"],
+  "migrations": ["dist/database/migrations/*.ts"],
   "entities": ["dist/entities/*.js"],
   "cli": {
     "migrationsDir": "dist/database/migrations",
     "entitiesDir": "dist/entities"
   }
 }
+
+process.env.MODE === "PRODUCTION" ?
+  module.exports = PRODUCTION :
+  module.exports = DEVELOPMENT
